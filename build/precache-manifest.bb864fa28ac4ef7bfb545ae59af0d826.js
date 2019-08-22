42506f15501d1863f59f2ef6793638675c9dac71
Nick Zaicev <mzaitsau@us.ibm.com>
2019-08-22 21:39:46 +0300

    add local build

---
+++ build/precache-manifest.bb864fa28ac4ef7bfb545ae59af0d826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7567e2e3cdcf5da8f681",
+    "url": "/static/js/main.ead031ea.chunk.js"
+  },
+  {
+    "revision": "17e5dac33d95ba06a47f",
+    "url": "/static/js/2.e880ce09.chunk.js"
+  },
+  {
+    "revision": "7567e2e3cdcf5da8f681",
+    "url": "/static/css/main.11740f2d.chunk.css"
+  },
+  {
+    "revision": "7f83108139318e108a6c24a41c983dc0",
+    "url": "/index.html"
+  }
+];
